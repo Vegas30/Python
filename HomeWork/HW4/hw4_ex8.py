f7049cb5040ea9d6bf99f16c488118a9084da6d9
Vegas30 <i_valuiskov@mail.ru>
2024-04-10 20:58:00 +0400

    hw4 все задачи и 8,9

--- HomeWork/HW4/hw4_ex8.py
+++ HomeWork/HW4/hw4_ex8.py
@@ -4,6 +4,88 @@
 # б) А и не (В и не С);
 # в) не (не А или В и С).
 
+print("Первый способ решения задачи:")
+
+print("а) не (А или не В и С):")
+A, B, C = True, True, True
+result = not (A or (not B and C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+A, B, C = True, True, False
+result = not (A or (not B and C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+A, B, C = True, False, True
+result = not (A or (not B and C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+A, B, C = True, False, False
+result = not (A or (not B and C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+A, B, C = False, True, True
+result = not (A or (not B and C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+A, B, C = False, True, False
+result = not (A or (not B and C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+A, B, C = False, False, True
+result = not (A or (not B and C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+A, B, C = False, False, False
+result = not (A or (not B and C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+
+print("б) А и не (В и не С):")
+A, B, C = True, True, True
+result = A and (not (B and not C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+A, B, C = True, True, False
+result = A and (not (B and not C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+A, B, C = True, False, True
+result = A and (not (B and not C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+A, B, C = True, False, False
+result = A and (not (B and not C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+A, B, C = False, True, True
+result = A and (not (B and not C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+A, B, C = False, True, False
+result = A and (not (B and not C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+A, B, C = False, False, True
+result = A and (not (B and not C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+A, B, C = False, False, False
+result = A and (not (B and not C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+
+print("не (не А или В и С):")
+A, B, C = True, True, True
+result = not (not A or (B and C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+A, B, C = True, True, False
+result = not (not A or (B and C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+A, B, C = True, False, True
+result = not (not A or (B and C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+A, B, C = True, False, False
+result = not (not A or (B and C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+A, B, C = False, True, True
+result = not (not A or (B and C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+A, B, C = False, True, False
+result = not (not A or (B and C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+A, B, C = False, False, True
+result = not (not A or (B and C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+A, B, C = False, False, False
+result = not (not A or (B and C))
+print(f"A={A}, B={B}, C={C}, результат={result}")
+
+print("\nВторой способ решения задачи:")
+
 print("а) не (А или не В и С):")
 for A in [True, False]:
     for B in [True, False]:
@@ -15,7 +97,7 @@ print("б) А и не (В и не С):")
 for A in [True, False]:
     for B in [True, False]:
         for C in [True, False]:
-            result = A and not (B and not C)
+            result = A and (not (B and not C))
             print(f"A={A}, B={B}, C={C}, результат={result}")
 
 print("в) не (не А или В и С):")
--- HomeWork/HW4/hw4_ex9.py
+++ HomeWork/HW4/hw4_ex9.py
@@ -4,8 +4,92 @@
 # б) не (А и не В) или (А или не С);
 # в) А и не В или не (А или не С).
 
+print("Первый способ решения задачи:")
+
+print("\nа) не (А и В) и (не А или не С):")
+print("A\t\tB\t\tC\t\tРезультат")
+A, B, C = True, True, True
+result = not (A and B) and (not A or (not C))
+print(f"{A}\t{B}\t{C}\t{result}")
+A, B, C = True, True, False
+result = not (A and B) and (not A or (not C))
+print(f"{A}\t{B}\t{C}\t{result}")
+A, B, C = True, False, True
+result = not (A and B) and (not A or (not C))
+print(f"{A}\t{B}\t{C}\t{result}")
+A, B, C = True, False, False
+result = not (A and B) and (not A or (not C))
+print(f"{A}\t{B}\t{C}\t{result}")
+A, B, C = False, True, True
+result = not (A and B) and (not A or (not C))
+print(f"{A}\t{B}\t{C}\t{result}")
+A, B, C = False, True, False
+result = not (A and B) and (not A or (not C))
+print(f"{A}\t{B}\t{C}\t{result}")
+A, B, C = False, False, True
+result = not (A and B) and (not A or (not C))
+print(f"{A}\t{B}\t{C}\t{result}")
+A, B, C = False, False, False
+result = not (A and B) and (not A or (not C))
+print(f"{A}\t{B}\t{C}\t{result}")
+
+print("\nб) не (А и не В) или (А или не С):")
+print("A\t\tB\t\tC\t\tРезультат")
+A, B, C = True, True, True
+result = not (A and (not B)) or (A or (not C))
+print(f"{A}\t{B}\t{C}\t{result}")
+A, B, C = True, True, False
+result = not (A and (not B)) or (A or (not C))
+print(f"{A}\t{B}\t{C}\t{result}")
+A, B, C = True, False, True
+result = not (A and (not B)) or (A or (not C))
+print(f"{A}\t{B}\t{C}\t{result}")
+A, B, C = True, False, False
+result = not (A and (not B)) or (A or (not C))
+print(f"{A}\t{B}\t{C}\t{result}")
+A, B, C = False, True, True
+result = not (A and (not B)) or (A or (not C))
+print(f"{A}\t{B}\t{C}\t{result}")
+A, B, C = False, True, False
+result = not (A and (not B)) or (A or (not C))
+print(f"{A}\t{B}\t{C}\t{result}")
+A, B, C = False, False, True
+result = not (A and (not B)) or (A or (not C))
+print(f"{A}\t{B}\t{C}\t{result}")
+A, B, C = False, False, False
+result = not (A and (not B)) or (A or (not C))
+print(f"{A}\t{B}\t{C}\t{result}")
+
+print("\nв) А и не В или не (А или не С):")
+print("A\t\tB\t\tC\t\tРезультат")
+A, B, C = True, True, True
+result = A and (not B) or (not (A or (not C)))
+print(f"{A}\t{B}\t{C}\t{result}")
+A, B, C = True, True, False
+result = A and (not B) or (not (A or (not C)))
+print(f"{A}\t{B}\t{C}\t{result}")
+A, B, C = True, False, True
+result = A and (not B) or (not (A or (not C)))
+print(f"{A}\t{B}\t{C}\t{result}")
+A, B, C = True, False, False
+result = A and (not B) or (not (A or (not C)))
+print(f"{A}\t{B}\t{C}\t{result}")
+A, B, C = False, True, True
+result = A and (not B) or (not (A or (not C)))
+print(f"{A}\t{B}\t{C}\t{result}")
+A, B, C = False, True, False
+result = A and (not B) or (not (A or (not C)))
+print(f"{A}\t{B}\t{C}\t{result}")
+A, B, C = False, False, True
+result = A and (not B) or (not (A or (not C)))
+print(f"{A}\t{B}\t{C}\t{result}")
+A, B, C = False, False, False
+result = A and (not B) or (not (A or (not C)))
+print(f"{A}\t{B}\t{C}\t{result}")
+
+print("\nВторой способ решения задачи:")
 # Заголовки для результатов
-print("а) не (А и В) и (не А или не С):")
+print("\nа) не (А и В) и (не А или не С):")
 print("A\t\tB\t\tC\t\tРезультат")
 
 # Перебор всех комбинаций A, B и C
@@ -13,7 +97,7 @@ for A in [True, False]:
     for B in [True, False]:
         for C in [True, False]:
             # Вычисление выражения а)
-            result_a = not (A and B) and (not A or not C)
+            result_a = not (A and B) and (not A or (not C))
             print(f"{A}\t{B}\t{C}\t{result_a}")
 
 # Заголовки для результатов
@@ -25,7 +109,7 @@ for A in [True, False]:
     for B in [True, False]:
         for C in [True, False]:
             # Вычисление выражения б)
-            result_b = not (A and not B) or (A or not C)
+            result_b = not (A and (not B)) or (A or (not C))
             print(f"{A}\t{B}\t{C}\t{result_b}")
 
 # Заголовки для результатов
@@ -37,5 +121,5 @@ for A in [True, False]:
     for B in [True, False]:
         for C in [True, False]:
             # Вычисление выражения в)
-            result_c = A and not B or not (A or not C)
+            result_c = A and (not B) or (not (A or (not C)))
             print(f"{A}\t{B}\t{C}\t{result_c}")
