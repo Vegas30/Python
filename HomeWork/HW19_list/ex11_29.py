8059847101b57534d7bc1f7979585acd0c340f81
vegas from AMD <i_valuiskov@mail.ru>
2024-06-11 23:22:03 +0400

    hw19 in progress

--- HomeWork/HW19_list/ex11_29.py
+++ HomeWork/HW19_list/ex11_29.py
@@ -3,10 +3,26 @@
 # б) верно ли, что сумма квадратов элементов массива есть
 # пятизначное число.
 
+import random
 
-def summ_elem_is_even():
+
+def input_array(n):
+    array = []
+    for _ in range(n):
+        array.append(random.randint(0, 10))
+    return array
+
+def summ_elem_is_even(array):
+    summ = sum(array)
+    if summ % 2 == 0:
+        print(f"Верно, сумма элементов массива {summ} есть четное число")
+    else:
+        print(f"Не верно, сумма элементов массива {summ} не четное число")
 
 
 def main():
+    array_new = input_array(10)
+    print(array_new)
+    summ_elem_is_even(array_new)
 
 main()
