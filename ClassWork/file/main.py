963a2f291d6ae331004e1863b08aaf96726c1c78
Vegas30 <i_valuiskov@mail.ru>
2024-09-19 17:03:21 +0400

    ex_temperature.py - done, hw31 - done except 32 hw29-30 in progress

--- ClassWork/file/main.py
+++ ClassWork/file/main.py
@@ -1,64 +1,84 @@
-# Напишите программу которая читает текстовый файл и подсчитывает кол-во строк в нем.
+# 1. Напишите программу, которая читает текстовый файл и подсчитывает
+# количество строк в нем. Предварительно создайте файл и
+# запишите туда какие-либо данные.
 
-def appenf_lines(file_name) -> None:
-    try:
-        with open(file_name, 'w', encoding="utf-8") as file:
-            while (line := input("Введите строку (exit для выхода): ")) != 'exit':
-                file.write(line + '\n')
-    except FileNotFoundError as e:
-        print("Файл не найден.", e)
-    except (IOError, OSError) as e:
-        print("Ошибка ")
+# 2. Напишите программу, которая читает текстовый файл и подсчитывает количество слов
+# в нем.
 
+# 3. Напишите функцию, которая копирует содержимое одного файла в другой.
+import shutil
 
-def add_lines(file_name) -> None:
-    try:
-        with open(file_name, 'a', encoding="utf-8") as file:
-            while (line := input("Введите строку (exit для выхода): ")) != 'exit':
-                file.write(line + '\n')
-    except FileNotFoundError as e:
-        print("Файл не найден.", e)
-    except (IOError, OSError) as e:
-        print("Ошибка ")
+shutil.copy('exp.txt', 'copy_2.txt')
 
 
-def count_lines(file_name):
-    count = 0
-    with open(file_name, 'r') as file:
-        for line in file:
-            if line:
-                count += 1
-    return count
-
-
-def count_words(file_name):
-    count = 0
-    with open(file_name, 'r', encoding="utf-8") as file:
-        return sum(len(line.split()) for line in file)
-
-    return list_file
-
-# Напишите функцию которая копирует содержимое одного файла в другой
-def copy_file(file1, file2):
-    with open(file1, 'r', encoding="utf-8") as file:
-        content = file.read()
-    with open(file2, 'w', encoding="utf-8") as file:
-        file.write(content)
-
-def read_first_n_lines(file_name, n):
-    with open(file_name, 'r', encoding="utf-8") as file:
+# 4. Напишите функцию, которая читает первые n строк файла и выводит их на экран.
+def read_first_n_lines_z4(n: int) -> None:
+    with open('copy.txt', 'r', encoding='utf-8') as file:
         for _ in range(n):
             print(file.readline())
 
 
+def copy_file_z3(filename: str, filename2: str) -> None:
+    with open(filename, 'r', encoding='utf-8') as file:
+        content = file.read()
+    with open(filename2, 'w', encoding='utf-8') as file:
+        file.write(content)
+
+
+def count_words_z2():
+    try:
+        with open('exp.txt', 'r', encoding="utf-8") as file:
+            # words = 0
+            # for line in file:
+            #     words += len(line.split())
+            # return words
+            return sum(len(line.split()) for line in file)
+    except FileNotFoundError as e:
+        print("Файл не найден.", e)
+    except (IOError, OSError) as e:
+        print("Ошибка доступа или памяти", e)
+
+
+def append_lines(filename: str) -> None:
+    try:
+        with open(filename, 'w', encoding="utf-8") as file:
+            while (line := input("Введите строку (exit для выхода): ")) != 'exit':
+                file.write(line + '\n')
+    except PermissionError as e:
+        print("Нет прав к доступу или каталогу.", e)
+
+
+def add_lines(filename: str) -> None:
+    try:
+        with open(filename, 'a', encoding="utf-8") as file:
+            while (line := input("Введите строку (exit для выхода): ")) != 'exit':
+                file.write(line + '\n')
+    except PermissionError as e:
+        print("Нет прав к доступу или каталогу.", e)
+
+
+def count_lines_1z(filename: str) -> int:
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            return len(file.readlines())
+    except FileNotFoundError as e:
+        print("Файл не найден.", e)
+    except (IOError, OSError) as e:
+        print("Ошибка доступа или памяти", e)
+
+
 def main():
     filename = 'exp.txt'
-    filename2 = 'exp2.txt'
-    # appenf_lines(filename)
+    copy_file = input("Введите имя файла, в которой будет скопирован файл 'exp.txt'") + '.txt'
+    # append_lines(filename)
     # add_lines(filename)
-    # print(count_lines(filename))
-    # print(count_words(filename))
-    copy_file(filename, filename2)
+    print(f"Количество строк в файле: {count_lines_1z(filename)}")
+    print(f"Количество слов в файле: {count_words_z2()}")
+    copy_file_z3(filename, copy_file)
 
-if __name__ == '__main__':
+    n = int(input("Введите сколько первых строк вы хотите прочитать: "))
+    read_first_n_lines_z4(n)
+
+
+if __name__ == "__main__":
     main()
