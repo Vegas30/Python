a49218da267569d6a8feb85b0d6b331f1cd379c4
Student_04 <i_valuiskov@mail.ru>
2024-12-05 21:13:35 +0400

    ClassWork time of change

--- ClassWork/test.py
+++ ClassWork/test.py
@@ -1,1 +1,12 @@
-print()
+class Parent:
+    __slots__ = ('attr1',)
+
+class Child(Parent):
+    __slots__ = ('attr2',)
+
+obj = Child()
+obj.attr1 = "attr1"
+obj.attr2 = "attr2"
+
+print(obj.__slots__)
+print(obj.attr1)
