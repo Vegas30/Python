978999e5242ff689b1db8a891153be4d812cb8e6
Vegas30 <i_valuiskov@mail.ru>
2025-01-21 16:53:11 +0400

    SQL Homework

--- SQL/cw_2.sql
+++ SQL/cw_2.sql
@@ -19,31 +19,67 @@ SELECT city, COUNT(*) AS num_students
 FROM students
 group by city;
 -- 34. Выбрать все тесты, на которых не было ни одного студента:
-SELECT 
-    *
-FROM
-    tests
-WHERE
-    idTest NOT IN (SELECT 
-            tests_id
-        FROM
-            result);  
+SELECT * FROM tests WHERE idTest NOT IN (SELECT tests_id FROM result);
 -- Обновить специализацию студента с фамилией "Сидоров" на "Программирование":
+update students
+SET special_id = '1'
+where students.surname = 'Сидоров';
 -- 39. Выбрать имена всех студентов, у которых оценка по тестам выше 80:
--- 40. Выбрать имена всех преподавателей, проводивших уроки с аудиторией больше 30:
+select name from students join tests on special_id = tests.lesson_id where ball > 80;
+-- 40. Выбрать имена всех преподавателей, проводивших уроки с аудиторией больше 30: ???!!!
+
 -- 41. Найти все уроки, проведенные преподавателем с именем "Анна" или "Иван":
--- 42. Найти все специализации, в названии которых есть слово "IT":
--- 46. Выбрать все уроки, проведенные преподавателем с фамилией "Смирнов" и аудитория которых менее или равна 20:
--- 48. Найти все тесты, на которых было не менее 10 студентов:
--- 49. Найти количество уникальных специализаций среди студентов:
+select nameLessons from lessons join teachers on teacher_id = idTeachers where name = 'Анна' or name = 'Иван';
+select nameLessons from lessons join teachers on teacher_id = idTeachers where name IN ('Анна','Иван');
+-- 42. Найти все специализации, в названии которых есть слово "IT": ???!!! нет ни одной
+
+-- 46. Выбрать все уроки, проведенные преподавателем с фамилией "Смирнов" и аудитория которых менее или равна 20: ???!!!
+
+-- 48. Найти все тесты, на которых было не менее 10 студентов: ???!!!
+
+-- 49. Найти количество уникальных специализаций среди студентов: ???!!! EXCEPT - не работает? Не понял вопроса
+SELECT count(distinct students.special_id) as UniqSpec_Num FROM students;
 -- 51. Найти всех студентов, проживающих в городе "Санкт-Петербург" и у которых номер дома больше 100:
+-- SELECT name, right(adress, 3) as house_number from students; --  where city = 'Санкт-Петербург' AND house_number > 100);
+
+select name, substr(adress FROM 'д. ') as house_number from students;
+
+SELECT name, COUNT(*) AS house_number
+FROM teachers
+GROUP BY departments_id
+HAVING num_teachers > 5;
+
+
+-- SELECT *
+-- FROM students
+-- WHERE surname = 'Кузьмина' 
+-- AND region > (
+--     SELECT AVG(region)
+--     FROM students
+-- );
+
 -- 52. Посчитать количество студентов в каждом городе:
+select city, count(*) as StudentCount
+FROM students
+group by city;
 -- 61. Найти все отделы, в которых руководителем является преподаватель с фамилией "Иванов":
+select departments.nameOtdel from departments where bossOtdel LIKE 'Иванов %';
 -- 63. Посчитать количество уроков для каждого преподавателя:
+select concat_ws(' ', surname, name, otchestvo) as full_name, count(teacher_id) AS lessons_count
+from teachers
+join lessons ON idTeachers = teacher_id
+group by idTeachers;
 -- 64. Найти все специализации, на которых учатся студенты из города "Москва":
--- 73. Посчитать общее количество тестов, проведенных по каждому предмету:
+select concat_ws(' ', name, surname) as full_name, city, nameSpecial from students join specializations ON special_id = idSpecial where city = 'Москва';
+-- 73. Посчитать общее количество тестов, проведенных по каждому предмету: ???!!! Считает красиво но не правильно
+select nameLessons, count(lesson_id) as tests_count
+from lessons
+join tests on idLessons = lesson_id
+group by nameLessons;
 -- 74. Найти всех студентов, чьи фамилии содержат букву "е" и имеют длину не менее 5 символов:
+select * from students where surname LIKE '%е%' and character_length(surname) > 8;
 -- 75. Обновить данные о пройденных тестах: установить значение "Пройден" для всех тестов, на которых студенты получили оценку выше 3:
+
 -- 77. Посчитать количество студентов, зарегистрированных на каждый специализационный курс:
 -- 73. Посчитать количество преподавателей в каждом отделе:
 -- 75. Обновить данные о специализации студента с именем "Иван" и фамилией "Иванов":
--- SQL/test/hw_1.sql
+++ SQL/test/hw_1.sql
@@ -3,7 +3,7 @@ SELECT
     nameOtdel
 FROM
     departments; 
--- 2. Выбрать всех преподавателей: 
+-- 2. Выбрать всех преподавателей:
 SELECT 
     *
 FROM
@@ -19,16 +19,78 @@ SELECT
 FROM
     departments;
 -- 10. Выбрать имена и специализации всех студентов:
-select name, nameSpecial FROM students LEFT JOIN specializations ON special_id = idSpecial 
+SELECT 
+    name, nameSpecial
+FROM
+    students
+        JOIN
+    specializations ON special_id = idSpecial; 
 -- 11. Выбрать имена студентов и их адреса: 
-
+SELECT 
+    name, adress
+FROM
+    students;
 -- 13. Выбрать имена преподавателей и названия их уроков: 
+SELECT 
+    name, nameLessons
+FROM
+    teachers
+        JOIN
+    lessons ON idTeachers = teacher_id; 
 -- 14. Выбрать имена и должности всех преподавателей, у которых номер телефона начинается с "8": 
--- 15. Выбрать имена студентов, у которых регион равен 2: 
+SELECT 
+    name, post
+FROM
+    teachers
+WHERE
+    phone LIKE '8%';
+-- 15. Выбрать имена студентов, у которых регион равен 2:
+SELECT 
+    name
+FROM
+    students
+WHERE
+    region = 2;
 -- 16. Выбрать имена студентов и названия их специализаций, у которых номер телефона начинается с "9": 
--- 17. Выбрать имена отделов, в которых работает босс с фамилией "Иванов": 
+SELECT 
+    name, specializations.nameSpecial
+FROM
+    students
+        JOIN
+    specializations ON special_id = idSpecial
+WHERE
+    students.number LIKE '9%';
+-- 17. Выбрать имена отделов, в которых работает босс с фамилией "Иванов":
+SELECT 
+    nameOtdel
+FROM
+    departments
+WHERE
+    departments.bossOtdel LIKE 'Иванов %';  
 -- 18. Выбрать имена всех студентов в порядке убывания их имен: 
+SELECT 
+    name
+FROM
+    students
+ORDER BY name DESC;
 -- 19. Выбрать среднюю оценку по всем тестам: 
+SELECT 
+    AVG(ball) AS average_ball
+FROM
+    tests;
 -- 20. Выбрать количество студентов в каждом регионе: 
--- 21. Обновить должность преподавателя с id 5 на "Доцент": 
+SELECT 
+    region, COUNT(*) AS num_students
+FROM
+    students
+GROUP BY region;
+-- 21. Обновить должность преподавателя с id 5 на "Доцент":
+UPDATE teachers 
+SET 
+    post = 'Доцент'
+WHERE
+    idTeachers = 5;
 -- 22. Добавить нового студента:
+INSERT INTO test.students (idStudent, surname, name, adress, city, region, number, special_id) 
+VALUES ('31', 'Добавлялов', 'Новичок', 'ул. Новая, д.1', 'Астрахань', '30', '892711111', '11');
+-- delete from students where idStudent = 31;
