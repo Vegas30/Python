14ba27d7f32f3bd7421e0007f8cadc8d2e1b61b5
Vegas30 <i_valuiskov@mail.ru>
2025-02-19 21:35:39 +0400

    WareHouse Homework

--- HomeWork/WareHouse_Management/operations/validation.py
+++ HomeWork/WareHouse_Management/operations/validation.py
@@ -1,32 +1,9 @@
 # WareHouse_Management/operations/validation.py
 
+"""Модуль для валидации данных."""
+
 class ValidationError(Exception):
     pass
-
-
-
-
-# def validate_new_item_data(new_item: dict) -> None:
-#     if not new_item['item_id'].strip():
-#         raise ValidationError("Введите ID товара.")
-#
-#     if not new_item['name'].strip():
-#         raise ValidationError("Введите наименование товара.")
-#
-#     if not new_item['category'].strip():
-#         raise ValidationError("Введите категорию товара.")
-#
-#     if not isinstance(new_item['quantity'], int) or new_item['quantity'] < 0:
-#         raise ValidationError("'quantity' должно быть неотрицательным целым числом.")
-#
-#     if not isinstance(new_item['price'], (int, float)) or new_item["price"] < 0:
-#         raise ValidationError("'price' должен быть неотрицательным числом.")
-#
-#     if not new_item['tags']:
-#         raise ValidationError("Укажите хотя бы один 'tag'.")
-#
-#     if not new_item['locations']:
-#         raise ValidationError("Обязательно укажите 'locations'.")
 
 def validate_new_item_data(items: list[dict], new_item: dict) -> None:
     for item in items:
@@ -39,11 +16,11 @@ def validate_new_item_data(items: list[dict], new_item: dict) -> None:
     if not new_item['name'].strip():
         raise ValidationError("Введите наименование товара.")
 
-    if not new_item['category'].strip():
-        raise ValidationError("Введите категорию товара.")
+    # if not new_item['category'].strip():
+    #     raise ValidationError("Введите категорию товара.")
 
-    # if not isinstance(new_item['category'], str):
-    #     raise ValidationError("Ошибка: 'category' должна быть строкой.")
+    if not isinstance(new_item['category'], str):
+        raise ValidationError("Ошибка: 'category' должна быть строкой.")
 
     if not isinstance(new_item['quantity'], int) or new_item['quantity'] < 0:
         raise ValidationError("'quantity' должно быть неотрицательным целым числом.")
