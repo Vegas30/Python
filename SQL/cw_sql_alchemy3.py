b1347372a93469f96df74f6bc0f77a99024fc027
Student_04 <i_valuiskov@mail.ru>
2025-02-20 21:25:37 +0400

    ClassWork SQLAlchemy

--- SQL/cw_sql_alchemy3.py
+++ SQL/cw_sql_alchemy3.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 from sqlalchemy import Integer, Column, String, ForeignKey, Date, create_engine
 from datetime import date
+
 Base = declarative_base()
 
 
@@ -10,7 +11,8 @@ class Professor(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
 
-    course = relationship('Course', back_populates= 'professor')
+    course = relationship('Course', back_populates='professor')
+
 
 class Student(Base):
     __tablename__ = 'students'
@@ -21,26 +23,29 @@ class Student(Base):
     student_course = relationship('StudentsCourse', back_populates='student')
     courses = relationship('Student', secondary='StudentsCourse', back_populates='students')
 
+
 class Course(Base):
     __tablename__ = 'courses'
 
     id = Column(Integer, primary_key=True)
     title = Column(String(60), default=10)
-    professor_id = Column(Integer, ForeignKey('professors.id'), nullable= False)
+    professor_id = Column(Integer, ForeignKey('professors.id'), nullable=False)
 
     professor = relationship('Professor', back_populates='course')
 
     students = relationship('Course', secondary='StudentsCourse', back_populates='courses')
     student_course = relationship('StudentsCourse', back_populates='course')
 
+
 class StudentsCourse(Base):
     __tablename__ = 'students_courses'
-    student_id = Column(Integer, ForeignKey('students.id'), primary_key= True)
-    course_id = Column(Integer, ForeignKey('courses.id'), primary_key= True)
-    student_course_data = Column(Date, default = date.today())
+    student_id = Column(Integer, ForeignKey('students.id'), primary_key=True)
+    course_id = Column(Integer, ForeignKey('courses.id'), primary_key=True)
+    student_course_data = Column(Date, default=date.today())
 
     student = relationship('Student', back_populates='student_course')
     course = relationship('Course', back_populates='student_course')
+
 
 engine = create_engine('postgresql://postgres:12345678@localhost:5433/courses_manager')
 Base.metadata.create_all(engine)
@@ -62,13 +67,14 @@ try:
     student3 = Student(name="Pavel")
     student4 = Student(name="Sergey")
 
-    student_course1 = StudentsCourse(student = student1, course = course3)
-    student_course2 = StudentsCourse(student = student2, course = course3)
-    student_course3 = StudentsCourse(student = student3, course = course2)
-    student_course4 = StudentsCourse(student = student4, course = course1)
-    student_course5 = StudentsCourse(student = student1, course = course2)
+    student_course1 = StudentsCourse(student=student1, course=course3)
+    student_course2 = StudentsCourse(student=student2, course=course3)
+    student_course3 = StudentsCourse(student=student3, course=course2)
+    student_course4 = StudentsCourse(student=student4, course=course1)
+    student_course5 = StudentsCourse(student=student1, course=course2)
 
-    session.add_all([professor1,professor2,student1,student2,student3,student4,student_course1,student_course2,student_course3,student_course4,student_course5])
+    session.add_all([professor1, professor2, student1, student2, student3, student4, student_course1, student_course2,
+                     student_course3, student_course4, student_course5])
     session.commit()
 
 except Exception as e:
@@ -77,5 +83,3 @@ except Exception as e:
 
 finally:
     session.close()
-
-
